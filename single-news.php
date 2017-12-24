fd17acc6b22aa220449cbcc5b7ee493cd9bd4a94
Ancy <lazuliroseancy@gmail.com>
2017-12-24 22:36:20 +0900

    Fix Final modification

--- single-news.php
+++
@@ -1,7 +0,0 @@
-<?php while (have_posts()) : the_post(); ?>
-<div class="article">
-<h2><?php the_title(); ?></h2>
-<p class="date"><?php the_time('Y.m.d'); ?></p>
-<?php the_content(); ?>
-<!-- /.article --></div>
-<?php endwhile; ?>
